5bc0393b40b32d8b2a940f807d58ca2baf46ac7f
7sdev <319952691+7sdev@users.noreply.github.com>
2026-09-16 20:37:53 +0200

    Phase 0: CLI edr.py config + gestion d'erreurs (raise/config invalide)

--- argos_core/config.py
+++ argos_core/config.py
@@ -4,7 +4,7 @@ import os
 
 def load_config(): 
     data = {}
-    current_dir =  os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "config.json"))
+    current_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "config.json"))
 
     if(os.path.exists(current_dir)):
         try:
@@ -14,13 +14,13 @@ def load_config():
                     if key == "watch":
                         for path in data["watch"]:
                             if(os.path.exists(path) == False):
-                                print("The directory to monitor (set to “watch” in config.json) cannot be found or is unreadable") 
+                                raise RuntimeError(f"The directory to monitor (set to “watch” in config.json) cannot be found or is unreadable : [ {path} ]") 
 
         except json.JSONDecodeError as e:
-            print(f"The “config.json” files are not valid JSON documents.\n Error : {e}")
+            print(f"The “config.json” files are not valid JSON documents.\nError : {e}")
             raise
         except UnicodeDecodeError as e:
-            print(f"“config.json” does not contain any data encoded in UTF-8, UTF-16, or UTF-32.\n Error : {e}")   
+            print(f"“config.json” does not contain any data encoded in UTF-8, UTF-16, or UTF-32.\nError : {e}")   
             raise
     else:
         print("“config.json” not found")
--- edr.py
+++ edr.py
@@ -3,3 +3,35 @@
 argos-edr — Endpoint Detection & Response, CLI-first (Linux).
 """
 
+import argparse
+import sys
+import json
+import argos_core.config
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", nargs="?")
+    args = parser.parse_args()
+    
+    return args
+
+
+def main():
+    args = get_args()
+
+    try:
+        data = argos_core.config.load_config()
+    except (json.JSONDecodeError, UnicodeDecodeError, RuntimeError):
+        print("Invalid configuration; program terminated")
+        sys.exit(1)
+
+    if(args.config == "config"):
+        for key, value in data.items():
+            if(isinstance(value, list) == False):
+                print(f"[{key}] : {value}")
+                continue
+            for values in value:
+                    print(f"[{key}] : {values}")
+  
+if __name__ == "__main__":
+    main()
